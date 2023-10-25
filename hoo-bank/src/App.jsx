45e1cdb32529b13de9b4c43227c334f527b0755e
MelzCodes <matteo.melis2012@gmail.com>
2023-10-25 21:32:12 +0100

    Finishing template structure

--- hoo-bank/src/App.jsx
+++ hoo-bank/src/App.jsx
@@ -1,19 +1,12 @@
 import styles from './style'
 
-import { Navbar, Hero, Billing,
-  CardDeal,
-  Business,
-  Clients,
-  CTA,
-  Stats,
-  Footer,
-  Testimonials, } from './components';
+import { Navbar, Hero, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, } from './components';
 
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}> 
-          Navbar
+          <Navbar />
         </div>
       </div>
 
@@ -25,14 +18,14 @@ const App = () => (
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}> 
-          Stats
-          Business 
-          Billing 
-          CardDeal 
-          Testimonials
-          Clients 
-          CTA 
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials/>
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
 
